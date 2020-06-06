74bf1f6b5809089433ccc9d7a0202b9372156c4f
xufengbj <xfncwu@163.com>
2020-06-06 16:07:41 +0800

    dd

---
+++ ZYBaseUtilsTool/Classes/DYTestModel.m
@@ -0,0 +1,30 @@
+//
+//  DYTestModel.m
+//  DYCircleImage_Example
+//
+//  Created by 许锋 on 2020/5/29.
+//  Copyright © 2020 xufengbj. All rights reserved.
+//
+
+#import "DYTestModel.h"
+
+@implementation DYTestModel
+
+- (void)printName {
+    NSLog(@"姓名：许锋");
+}
+
+- (void)printAge {
+    NSLog(@"年龄：28");
+}
+
+- (void)printSchool {
+    NSLog(@"学校：清华大学");
+}
+
+- (void)addLibFramework {
+    NSLog(@"添加类库");
+}
+
+
+@end
